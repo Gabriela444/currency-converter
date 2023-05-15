21b3c6184e4561af7ef08d735786134587d4193f
Gabriela Małecka <malecka.gabriela02@gmail.com>
2023-05-15 13:36:19 +0200

    exchanged code into function

--- js/script.js
+++ js/script.js
@@ -1,68 +1,79 @@
-let formElement = document.querySelector(".js-form");
-let inputAmountElement = document.querySelector(".js-inputAmount");
-let inputCurrencyElement = document.querySelector(".js-inputCurrency");
-let outputCurrencyElement = document.querySelector(".js-outputCurrency");
-let resultElement = document.querySelector(".js-result");
+{
 
-let pln_eur = 0.2092;
-let pln_usd = 0.2041;
-let pln_gbp = 0.1834;
-let eur_usd = 0.9756;
-let eur_gbp = 0.8765;
-let usd_gbp = 0.8985;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let inputAmount = inputAmountElement.value;
-    let inputCurrency = inputCurrencyElement.value;
-    let outputCurrency = outputCurrencyElement.value;
-
-    switch (inputCurrency) {
-        case "PLN":
-            if (outputCurrency === "PLN")
-                result = (inputAmount * 1);
-            else if (outputCurrency === "EUR")
-                result = (inputAmount * pln_eur);
-            else if (outputCurrency === "USD")
-                result = (inputAmount * pln_usd);
-            else if (outputCurrency === "GBP")
-                result = (inputAmount * pln_gbp);
-            break;
-
-        case "EUR":
-            if (outputCurrency === "PLN")
-                result = (inputAmount / pln_eur);
-            else if (outputCurrency === "EUR")
-                result = (inputAmount * 1);
-            else if (outputCurrency === "USD")
-                result = (inputAmount * eur_usd);
-            else if (outputCurrency === "GBP")
-                result = (inputAmount * eur_gbp);
-            break;
-
-        case "USD":
-            if (outputCurrency === "PLN")
-                result = (inputAmount / pln_usd);
-            else if (outputCurrency === "EUR")
-                result = (inputAmount / eur_usd);
-            else if (outputCurrency === "USD")
-                result = (inputAmount * 1);
-            else if (outputCurrency === "GBP")
-                result = (inputAmount * usd_gbp);
-            break;
-
-        case "GBP":
-            if (outputCurrency === "PLN")
-                result = (inputAmount / pln_gbp);
-            else if (outputCurrency === "EUR")
-                result = (inputAmount / eur_gbp);
-            else if (outputCurrency === "USD")
-                result = (inputAmount / usd_gbp);
-            else if (outputCurrency === "GBP")
-                result = (inputAmount * 1);
-            break;
+    const chosenCurrency = () => {
+        const pln_eur = 0.2213;
+        const pln_usd = 0.2401;
+        const pln_gbp = 0.1927;
+        const eur_usd = 1.0938;
+        const eur_gbp = 0.8778;
+        const usd_gbp = 0.8026;
     }
-    resultElement.innerText = result.toFixed(2);
-});
 
+    const formInput = () => {
+
+        const inputAmountElement = document.querySelector(".js-inputAmount");
+        const inputCurrencyElement = document.querySelector(".js-inputCurrency");
+        const outputCurrencyElement = document.querySelector(".js-outputCurrency");
+        const resultElement = document.querySelector(".js-result");
+
+        const inputAmount = inputAmountElement.value;
+        const inputCurrency = inputCurrencyElement.value;
+        const outputCurrency = outputCurrencyElement.value;
+
+
+        switch (inputCurrency) {
+            case "PLN":
+                if (outputCurrency === "PLN")
+                    return inputAmount * 1;
+                else if (outputCurrency === "EUR")
+                    return inputAmount * pln_eur;
+                else if (outputCurrency === "USD")
+                    return inputAmount * pln_usd;
+                else if (outputCurrency === "GBP")
+                    return inputAmount * pln_gbp;
+
+            case "EUR":
+                if (outputCurrency === "PLN")
+                    return inputAmount / pln_eur;
+                else if (outputCurrency === "EUR")
+                    return inputAmount * 1;
+                else if (outputCurrency === "USD")
+                    return inputAmount * eur_usd;
+                else if (outputCurrency === "GBP")
+                    return inputAmount * eur_gbp;
+
+            case "USD":
+                if (outputCurrency === "PLN")
+                    return inputAmount / pln_usd;
+                else if (outputCurrency === "EUR")
+                    return inputAmount / eur_usd;
+                else if (outputCurrency === "USD")
+                    return inputAmount * 1;
+                else if (outputCurrency === "GBP")
+                    return inputAmount * usd_gbp;
+
+            case "GBP":
+                if (outputCurrency === "PLN")
+                    return inputAmount / pln_gbp;
+                else if (outputCurrency === "EUR")
+                    return inputAmount / eur_gbp;
+                else if (outputCurrency === "USD")
+                    return inputAmount / usd_gbp;
+                else if (outputCurrency === "GBP")
+                    return inputAmount * 1;
+        }
+
+
+        const resultTextElement = (inputAmount, inputCurrency, outputCurrency, pln_eur, pln_usd, pln_gbp, eur_usd, eur_gbp, usd_gbp, resultElement) => {
+            resultElement.innerText = `<i>${inputAmount} ${inputCurrency} ${outputCurrency} ${pln_eur} ${pln_usd} ${pln_gbp} ${eur_usd} ${eur_gbp} ${usd_gbp} = </i>${result.toFixed(2)}`;
+        }
+    }
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+    }
+
+    init();
+
+}
